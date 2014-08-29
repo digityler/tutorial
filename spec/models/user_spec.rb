74c5fb0e2c638a5edb2487a5581a79faf6e31e03
digityler <tysclark@gmail.com>
2014-08-29 15:38:52 -0400

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,7 +16,6 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
 
-
   it { should be_valid }
 
   describe "when name is not present" do
@@ -36,8 +35,7 @@ describe User do
 
   describe "when email format is invalid" do
     it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.foo@bar_baz.com foo@bar+baz.com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
@@ -84,19 +82,19 @@ describe User do
   end
 
   describe "return value of authenticate method" do
-	before { @user.save }
-	let(:found_user) { User.find_by(email: @user.email) }
+	  before { @user.save }
+	  let(:found_user) { User.find_by(email: @user.email) }
 
     describe "with valid password" do
-	  it { should eq found_user.authenticate(@user.password) }
-	end
+	    it { should eq found_user.authenticate(@user.password) }
+	  end
 
-	describe "with invalid password" do
-	  let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+	  describe "with invalid password" do
+	    let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 
-	  it { should_not eq user_for_invalid_password }
-	  specify { expect(user_for_invalid_password).to be_false }
-	end
+	    it { should_not eq user_for_invalid_password }
+	    specify { expect(user_for_invalid_password).to be_false }
+	  end
   end
 
 end
